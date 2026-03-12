e6e01c2069c1c02ed060a5dc791d4c301dda693b
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-03-12 20:50:55 +0600

    barchart added for initial analysis

--- src/components/ProductAnalysis/ProductAnalysis.jsx
+++ src/components/ProductAnalysis/ProductAnalysis.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, BarChart, Bar, XAxis, YAxis } from "recharts";
 
 const COLORS = ["#EF8411", "#F97316", "#FDBA74", "#FB923C"];
 
 const ProductAnalysis = () => {
   const [data, setData] = useState([]);
+  const [stats, setStats] = useState([]);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -35,6 +36,21 @@ const ProductAnalysis = () => {
       });
   }, []);
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    fetch("http://127.0.0.1:5000/api/product-stats", {
+      headers:{
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(res => res.json())
+    .then(stats => {
+      setStats(stats);
+    });
+
+  }, []); // important
+
   return (
     <div className="bg-orange-50 rounded-2xl py-12 px-4 md:px-8">
 
@@ -52,7 +68,7 @@ const ProductAnalysis = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
-          {/* Total Value Chart */}
+          {/* TOTAL VALUE PIE CHART */}
           <div className="bg-white rounded-2xl p-6 shadow-lg border border-orange-100">
             <h3 className="text-lg font-semibold text-gray-700 mb-6">
               TOTAL VALUE BY ITEM
@@ -82,15 +98,26 @@ const ProductAnalysis = () => {
             )}
           </div>
 
-          {/* Quantity Placeholder */}
+          {/* TOTAL QUANTITY BAR CHART */}
           <div className="bg-white rounded-2xl p-6 shadow-lg border border-orange-100">
             <h3 className="text-lg font-semibold text-gray-700 mb-6">
               TOTAL QUANTITY BY ITEM
             </h3>
 
-            <div className="h-80 flex items-center justify-center text-orange-400 font-medium">
-              Coming Soon
-            </div>
+            {stats.length === 0 ? (
+              <div className="h-80 flex items-center justify-center text-gray-400">
+                No Data
+              </div>
+            ) : (
+              <ResponsiveContainer width="100%" height={300}>
+                <BarChart data={stats}>
+                  <XAxis dataKey="product" />
+                  <YAxis />
+                  <Tooltip />
+                  <Bar dataKey="sales_count" fill="#EF8411" />
+                </BarChart>
+              </ResponsiveContainer>
+            )}
           </div>
 
         </div>
